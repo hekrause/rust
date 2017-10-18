98d9d291e8a2fb3f1968cca6347194818e616ff8
hekrause <hekrause@htwg-konstanz.de>
2017-10-18 23:21:57 +0200

    Trying these tests again.

--- exercises/pascals-triangle/src/lib.rs
+++ exercises/pascals-triangle/src/lib.rs
@@ -1,11 +1,34 @@
-pub struct PascalsTriangle;
+use std::iter::repeat;
+
+pub struct PascalsTriangle {
+    rows: Vec<Vec<u32>>
+}
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!();
+        let mut master_vec: Vec<Vec<u32>> = Vec::with_capacity(row_count as usize);
+
+        if row_count > 0 {
+            master_vec.push(vec![0, 1, 0]);
+
+            for i in 1..row_count as usize {
+                let mut vec: Vec<u32> = repeat(0).take(i + 3).collect();
+
+                for j in 1..((i + 3) - 1) {
+                    vec[j] = master_vec[i - 1][j - 1] + master_vec[i - 1][j]
+                }
+
+                master_vec.push(vec);
+            }
+
+            master_vec.iter_mut().for_each(|x| x.retain(|&y| y != 0));
+        }
+
+        PascalsTriangle { rows: master_vec }
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        self.rows.clone()
     }
 }
+
--- exercises/pascals-triangle/tests/pascals-triangle.rs
+++ exercises/pascals-triangle/tests/pascals-triangle.rs
@@ -10,7 +10,6 @@ fn no_rows() {
 }
 
 #[test]
-#[ignore]
 fn one_row() {
     let pt = PascalsTriangle::new(1);
     let expected: Vec<Vec<u32>> = vec![vec![1]];
@@ -18,7 +17,6 @@ fn one_row() {
 }
 
 #[test]
-#[ignore]
 fn two_rows() {
     let pt = PascalsTriangle::new(2);
     let expected: Vec<Vec<u32>> = vec![vec![1], vec![1, 1]];
@@ -26,7 +24,6 @@ fn two_rows() {
 }
 
 #[test]
-#[ignore]
 fn three_rows() {
     let pt = PascalsTriangle::new(3);
     let expected: Vec<Vec<u32>> = vec![vec![1], vec![1, 1], vec![1, 2, 1]];
@@ -34,7 +31,6 @@ fn three_rows() {
 }
 
 #[test]
-#[ignore]
 fn five_rows() {
     let pt = PascalsTriangle::new(5);
     let expected: Vec<Vec<u32>> = vec![vec![1],
@@ -46,7 +42,6 @@ fn five_rows() {
 }
 
 #[test]
-#[ignore]
 fn six_rows() {
     let pt = PascalsTriangle::new(6);
     let expected: Vec<Vec<u32>> = vec![vec![1],
@@ -59,7 +54,6 @@ fn six_rows() {
 }
 
 #[test]
-#[ignore]
 fn seven_rows() {
     let pt = PascalsTriangle::new(7);
     let expected: Vec<Vec<u32>> = vec![vec![1],
@@ -73,7 +67,6 @@ fn seven_rows() {
 }
 
 #[test]
-#[ignore]
 fn ten_rows() {
     let pt = PascalsTriangle::new(10);
     let expected: Vec<Vec<u32>> = vec![vec![1],
@@ -90,7 +83,23 @@ fn ten_rows() {
 }
 
 #[test]
-#[ignore]
+#[should_panic(expected = "attempt to add with overflow")]
+fn middle_numbers_getting_to_big_for_u32() {
+    PascalsTriangle::new(36);
+}
+
+#[test]
+fn last_of_35_rows() {
+    let pt = PascalsTriangle::new(35);
+    let expected: Vec<u32> =
+        vec![1, 34, 561, 5984, 46376, 278256, 1344904, 5379616, 18156204, 52451256, 131128140,
+             286097760, 548354040, 927983760, 1391975640, 1855967520, 2203961430, 2333606220,
+             2203961430, 1855967520, 1391975640, 927983760, 548354040, 286097760, 131128140,
+             52451256, 18156204, 5379616, 1344904, 278256, 46376, 5984, 561, 34, 1];
+    assert_eq!(expected, pt.rows().pop().unwrap());
+}
+
+#[test]
 fn last_of_four_rows() {
     let pt = PascalsTriangle::new(4);
     let expected: Vec<u32> = vec![1, 3, 3, 1];
